df14da2729a93f6bb7d2c45060cfc80ff1a82e6c
Sasha <sanaopan4ez@gmail.com>
2020-11-18 13:57:29 +0300

    fix: remove pagination wrapper

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -3,19 +3,19 @@ import './pagination.css';
 
 export const Pagination = ({ handlePrev, handleNext }) => {
   return (
-    <div className='pagination'>
+    <>
       <button
-        className='pagination__btn'
+        className='pagination__btn pagination__btn--left'
         onClick={handlePrev}
       >
         {'<'}
       </button>
       <button
-        className='pagination__btn'
+        className='pagination__btn pagination__btn--right'
         onClick={handleNext}
       >
         {'>'}
       </button>
-    </div>
+    </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 
 import { App } from './App';
 
-const imageAdress = 'https://media.macphun.com/img/uploads/customer/how-to/579/15531840725c93b5489d84e9.43781620.jpg?q=85&w=1340';
+const imageAdress =
+  'https://media.macphun.com/img/uploads/customer/how-to/579/15531840725c93b5489d84e9.43781620.jpg?q=85&w=1340';
 const text = `T h e G i f t o f t h e M a g i
 ONE DOLLAR AND EIGHTY-SEVEN CENTS.
 That was all. She had put it aside, one cent and then another and then
@@ -17,12 +18,26 @@ look at the home. Furnished rooms at a cost of $8 a week. There is little more t
 In the hall below was a letter-box too small to hold a letter. There
 was an electric bell, but it could not make a sound. Also there was a
 name beside the door: “Mr. James Dillingham Young.”`;
-const secondImg = 'https://images.assetsdelivery.com/compings_v2/biletskiy/biletskiy1506/biletskiy150600086.jpg'
+const secondImg =
+  'https://images.assetsdelivery.com/compings_v2/biletskiy/biletskiy1506/biletskiy150600086.jpg';
+const thirdImg =
+  'https://expertphotography.com/wp-content/uploads/2011/06/how-to-take-good-pictures-waterlilly.jpg';
+const video = 'https://youtu.be/P3cffdsEXXw';
 
 const content = [
   <div>{text}</div>,
-  <img style={{ width: '100%' }} src={imageAdress} alt='A Mysterious girl' />,
-  <img style={{ width: '100%' }} src={secondImg} alt='Beautiful field' />,
-]
+  <img style={{ width: '100%' }} src={imageAdress} alt="A Mysterious girl" />,
+  <img style={{ width: '100%' }} src={secondImg} alt="Beautiful field" />,
+  <img style={{ width: '100%' }} src={thirdImg} alt="a flower" />,
+  <iframe
+    style={{ position: 'relative', zIndex: 99999}}
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/P3cffdsEXXw"
+    frameBorder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen
+  ></iframe>,
+];
 
 ReactDOM.render(<App content={content} />, document.getElementById('root'));
